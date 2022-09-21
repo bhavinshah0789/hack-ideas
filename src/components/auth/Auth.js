93f6cb2962b0a7c066877d0d3c55d6f23c384c31
bhavin.shah <bhavin.shah@zeuslearning.com>
2022-09-22 03:29:10 +0530

    Create login and sign up ui

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,9 +1,74 @@
-
+import { useState, Fragment } from 'react';
+// components
 import Header from '../header/Header';
+// css classes
+import classes from './Auth.module.css';
+// constants
+import AuthConstants from '../../data/AuthConstants';
 
-const Auth = () => {  
+const Auth = () => {
+  const [submitButtonCaption, setSubmitButtonCaption] = useState(AuthConstants.loginText);
+  const [formCaption, setFormCaption] = useState(AuthConstants.signupInsteadText);
+  const [formMode, setFormMode] = useState(AuthConstants.loginMode);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const emailPattern = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+";
+  const passwordPattern = "^[A-Za-z0-9]{6,}$";
+
+  const updateEmail = (event) => {
+    setEmail(event.target.value.trim());
+  };
+
+  const updatePassword = (event) => {
+    setPassword(event.target.value.trim());
+  };
+
+  const updateButtonCaption = () => {
+    if (formCaption === AuthConstants.signupInsteadText) {
+      setSubmitButtonCaption(AuthConstants.signupText);
+      setFormCaption(AuthConstants.loginInstead);
+      setFormMode(AuthConstants.signupMode);
+    } else {
+      setSubmitButtonCaption(AuthConstants.loginText);
+      setFormCaption(AuthConstants.signupInsteadText);
+      setFormMode(AuthConstants.loginMode);
+    }
+  };
+
   return (
-    <Header />
+    <Fragment>
+        <Header />
+        <main className={classes.auth}>
+          <section>
+            <form>
+              <div className={classes.control}>
+                <label htmlFor='email'>{AuthConstants.emailText}</label>
+                <input
+                  type='email'
+                  id='email'
+                  pattern={emailPattern}
+                  value={email}
+                  onChange={updateEmail}
+                />
+              </div>
+              <div className={classes.control}>
+                <label htmlFor='password'>{AuthConstants.passwordText}</label>
+                <input
+                  type='password'
+                  id='password'
+                  pattern={passwordPattern}
+                  value={password}
+                  onChange={updatePassword}
+                />
+              </div>
+              <div className={classes.control}>
+                <button mode={formMode} type="submit">{submitButtonCaption}</button>
+                <button type="button" onClick={updateButtonCaption}>{formCaption}</button>
+              </div>
+            </form>
+          </section>
+        </main>
+      </Fragment>
   );
 };
 
---
+++ src/data/AuthConstants.js
@@ -0,0 +1,12 @@
+const AuthConstants = {
+  emailText: "Email",
+  passwordText: "Password",
+  loginText: "Login",
+  signupText: "Signup",
+  signupInsteadText: "Signup instead",
+  loginInstead: "Login instead",
+  loginMode: "login",
+  signupMode: "signup",
+};
+
+export default Object.freeze(AuthConstants);
