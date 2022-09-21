891e9400718b544f91ef363e887ca77861f06655
bhavin.shah <bhavin.shah@zeuslearning.com>
2022-09-22 04:06:27 +0530

    Add header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux';
 // css classes
 import classes from './Header.module.css';
 // constants
 import HeaderConstants from '../../data/HeaderConstants';
-
+// store
+import { authActions } from '../../store/auth-store';
 
 const Header = (props) => {
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  const logout = () => {
+    dispatch(authActions.logout());
+  };
+
   return (
     <header className={classes.header}>
       <h1>{HeaderConstants.pageHeaderText}</h1>
+      {isAuthenticated && 
+        <nav>
+          <ul>
+            <li>
+              {
+                !props.hideAddEventBtn &&
+                <button
+                  type="button"
+                >
+                  {HeaderConstants.addEventText}
+                </button>
+              }
+            </li>
+            <li>
+              <button
+                type="button"
+                onClick={logout}
+              >
+                {HeaderConstants.logoutText}
+              </button>
+            </li>
+          </ul>
+        </nav>
+      }
+      
     </header>
   );
 };
--- src/data/HeaderConstants.js
+++ src/data/HeaderConstants.js
@@ -1,5 +1,7 @@
 const HeaderConstants = {
   pageHeaderText: "Hack Ideas",
+  logoutText: "Logout",
+  addEventText: "Add Event",
 };
 
 export default Object.freeze(HeaderConstants);
