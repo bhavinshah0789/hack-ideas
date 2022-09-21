2f09b93d3ef636f8497b1480cf8be7bb2094d6c0
bhavin.shah <bhavin.shah@zeuslearning.com>
2022-09-22 03:44:10 +0530

    Create a spinner

---
+++ src/components/base-spinner/BaseSpinner.js
@@ -0,0 +1,21 @@
+// css classes
+import classes from './BaseSpinner.module.css';
+
+const BaseSpinner = () => {
+  return (
+    <div className={classes.spinner}>
+      <div className={classes['lds-roller']}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default BaseSpinner;
