2ba6d410e1cae22be971e807017fe6e8df5ae27a
bhavin.shah <bhavin.shah@zeuslearning.com>
2022-09-22 04:31:58 +0530

    Create a event page

---
+++ src/components/event-page/EventPage.js
@@ -0,0 +1,11 @@
+// components
+import EventSortFilter from '../event-sort-filter/EventSortFilter';
+
+const EventPage = () => {
+
+  return (
+    <EventSortFilter />
+  );
+};
+
+export default EventPage;
